5b3af6eada2868c143efff5e197b48f8e18bd4fa
Shikovich95 <shehabeldeen.ab@gmail.com>
2023-04-07 16:53:48 +0200

    Remove the ProfileCard data array

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,14 @@
-import ProfileCard from "./components/ProfileCard"
+import ProfileCard from "./components/ProfileCard";
 
 function App() {
-  const data = [
-    {person1: {
-      title: "Alexa",
-      text: "Test"
-    }},
-    {person2: {
-      title: "cort",
-      text: "Testsss"
-    }},
-  ]
-  
   return (
     <div>
       <div className="pannel">Personal Digital Assistants</div>
-      <ProfileCard dataProp = {data} />
+      <ProfileCard />
       <ProfileCard />
       <ProfileCard />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
