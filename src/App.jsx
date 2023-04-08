5a9321d9dd31c4e28480e2259bc06f7b733dbd67
unknown <shehabeldeen.ab@gmail.com>
2023-04-08 14:14:23 +0200

    add some styles with tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import ProfileCard from "./components/ProfileCard";
+import AlexaImage from "./assets/images/alexa.png";
+import CortanaImage from "./assets/images/cortana.png";
+import SiriImage from "./assets/images/siri.png";
 
 function App() {
   return (
-    <div className="text-3xl font-bold underline">
-      <div className="font-bold text-center">Personal Digital </div>
-      <ProfileCard />
-      <ProfileCard />
-      <ProfileCard />
+    <div className="p-5 text-center ">
+      <h1 className="bg-orange-400 rounded-full text-center w-full font-bold">
+        Personal Digital Assistance
+      </h1>
+      <br />
+      <div className="flex justify-around ">
+        <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
+        <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImage} />
+        <ProfileCard title="Siri" handle="@siri01" image={SiriImage} />
+      </div>
     </div>
   );
 }
--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 function ProfileCard(props) {
-  return <div>hh</div>;
+  const { title, handle, image } = props;
+  return (
+    <div className="columns-1  rounded bg-zinc-50 p-2">
+      <img src={image} alt="Logo" className="w-full" />
+      <h1> {title}</h1>
+      <p className="underline"> {handle} </p>
+    </div>
+  );
 }
 
 export default ProfileCard;
